d75d30a2517f25d6cbaa98f18150f4c4462957b7
Chasky91 <david_c91@outlook.com>
2024-09-20 18:06:56 -0300

    primero pasos

---
+++ alumnos.js
@@ -0,0 +1,74 @@
+export default alumnos = [
+    {
+      "id_alumno": 1,
+      "nombre": "Juan",
+      "apellido": "Pérez",
+      "gmail": "juan.perez@gmail.com",
+      "id_curso":0
+    },
+    {
+      "id_alumno": 2,
+      "nombre": "María",
+      "apellido": "Gómez",
+      "gmail": "maria.gomez@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 3,
+      "nombre": "Carlos",
+      "apellido": "López",
+      "gmail": "carlos.lopez@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 4,
+      "nombre": "Ana",
+      "apellido": "Martínez",
+      "gmail": "ana.martinez@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 5,
+      "nombre": "Luis",
+      "apellido": "Fernández",
+      "gmail": "luis.fernandez@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 6,
+      "nombre": "Sofía",
+      "apellido": "Ramírez",
+      "gmail": "sofia.ramirez@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 7,
+      "nombre": "Pedro",
+      "apellido": "García",
+      "gmail": "pedro.garcia@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 8,
+      "nombre": "Laura",
+      "apellido": "Torres",
+      "gmail": "laura.torres@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 9,
+      "nombre": "Diego",
+      "apellido": "Sánchez",
+      "gmail": "diego.sanchez@gmail.com",
+      "id_curso": 0
+    },
+    {
+      "id_alumno": 10,
+      "nombre": "Clara",
+      "apellido": "Ortiz",
+      "gmail": "clara.ortiz@gmail.com",
+      "id_curso": 0
+    }
+  ]
+  
+  
---
+++ operadorAlumno.js
@@ -0,0 +1,4 @@
+import alumnos from "./alumnos.js" //pseudo base de datos
+
+// Mostrar los datos de los alumnos por la teminal
+
