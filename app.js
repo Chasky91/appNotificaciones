5b8e1a207d0986713d481e7b24ea6fd35ddb1617
Chasky91 <david_c91@outlook.com>
2024-10-14 19:31:18 -0300

    actualizacion appNotificacino

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import alumnos from './alumnos.js'
 
 //creo el servidor
 const app = express()
@@ -10,7 +11,23 @@ const app = express()
 app.get('/alumnos', (req, res) => {
     
     //res.send() devuelve una respuesta al usuario con los datos solicitados
-    res.send("devuelve todos los alumnos")
+    res.json({"mensanje": alumnos})
+})
+
+//localhost:5000"/alumnos/id_alumno"
+app.get('/alumnos/:id_alumno', (req, res) => {
+
+    let id = req.params.id_alumno //conversion de datos, poorque esta variable es un string
+    console.log(id)
+    for(let i = 0; i < alumnos.length; i++ ) {
+
+        if(id===alumnos[i].id_alumno){ 
+            return console.log(alumnos[i])
+        } else {
+            console.log("accede a la posicion pero no encuentra nada en el indice Nº", i)
+        }
+    }
+    res.json({"mensanje": "Es te endpoint solo devuelve una Alumno"})
 })
 
 //localhost:5000/alumnos/:id
