8cc8f3d5cb5965690ca505e26f70ee7290d5aba5
Chasky91 <david_c91@outlook.com>
2024-11-15 11:55:03 -0300

    cambio materia

--- sql/scripts/notidicaciones_admi_alumn.sql
+++ sql/scripts/notidicaciones_admi_alumn.sql
@@ -3,7 +3,7 @@ DROP TABLE if exists notifica_adm_alum;
 CREATE TABLE notifica_adm_alum (
 	id_admin INT,
     dni_alum INT,
-    id_profe INT,
+    id_materia INT,
     fecha_msj DATE DEFAULT(current_timestamp),
     msj VARCHAR(200) NOT NULL,
     PRIMARY KEY(id_admin, dni_alum, id_profe),
